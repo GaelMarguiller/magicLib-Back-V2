425d0acf9d6bf595e913ddadd52218ad634b0441
GMarguiller <gael.marguiller@supinternet.fr>
2020-11-18 08:07:12 +0100

    add login route

---
+++ src/routes/loginRoute.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import passport from 'passport';
+import { UserNotFoundError } from '../controllers/errors/userNotFound';
+
+const router = Router();
+
+router.post('/', (req, res) => {
+    const authenticationFunction = passport.authenticate('local', (err, user) => {
+        if (err) {
+            if (err instanceof UserNotFoundError) return res.status(404).send('User not found');
+            return res.status(500).send();
+        }
+
+        if (user) {
+            return req.logIn(user, (error) => {
+                if (error) return res.status(500).send();
+                return res.send(user.getSafeUser());
+            });
+        }
+        return res.status(404).send('User not found');
+    });
+
+    return authenticationFunction(req, res);
+});
+
+export default router;
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import usersRoutes from './usersRoute';
+import loginRoutes  from './loginRoute';
 
 const router = Router();
 
 router.use('/users', usersRoutes);
+router.use('/login', loginRoutes);
 
 export default router;
